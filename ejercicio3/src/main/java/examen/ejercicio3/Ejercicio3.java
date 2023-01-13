0ef564a0de1c53dd0ec4fa84f31cd3bf7ddfc6cc
Kangelx <angel.garciagarcia@iesmiguelherrero.com>
2023-01-13 18:06:00 +0100

    commit desde bash

--- ejercicio3/src/main/java/examen/ejercicio3/Ejercicio3.java
+++ ejercicio3/src/main/java/examen/ejercicio3/Ejercicio3.java
@@ -12,6 +12,10 @@ package examen.ejercicio3;
 public class Ejercicio3 {
 
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        String frase = "Me llamo Angel García";
+        int conEspacios= frase.length();
+        System.out.println("Longitud de la frase con espacios: "+conEspacios);
+        int sinEspacios=frase.replace(" ","").length();
+        System.out.println("La frase tiene: "+sinEspacios+"letras");
     }
 }
